fe9dc43c21fcbbfcd3ea5d12a839296ab6148c39
jlhonora <jlhonora@ing.puc.cl>
2014-05-27 01:48:24 +0000

    Uncomment dependencies

--- rpi/utils/install-dependencies.sh
+++ rpi/utils/install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#sudo apt-get -y install redis-server postgresql libpq-dev python-setuptools
-#sudo easy_install pip
+sudo apt-get -y install redis-server postgresql libpq-dev python-setuptools
+sudo easy_install pip
 
 sudo pip install pyserial rq psycopg2 pyyaml python-twitter schedule
