f9928aec7a2182d8b6a5442009c1e149b618ad57
jlhonora <jlhonora@ing.puc.cl>
2014-09-20 20:07:24 -0300

    Added google APIs to deps

--- rpi/utils/install-dependencies.sh
+++ rpi/utils/install-dependencies.sh
@@ -3,4 +3,4 @@
 sudo apt-get -y install redis-server postgresql libpq-dev python-setuptools
 sudo easy_install pip
 
-sudo pip install pyserial rq psycopg2 pyyaml python-twitter schedule
+sudo pip install pyserial rq psycopg2 pyyaml python-twitter schedule google-api-python-client
