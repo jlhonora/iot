90f7542d1bd1a8264dfc55cf464deb0351729e9c
jlhonora <jlhonora@ing.puc.cl>
2014-09-20 17:58:42 -0300

    Added logic for uploaded videos

--- rpi/camera_director.py
+++ rpi/camera_director.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
-import time
+import camera_manager
+from dateutil import parser
+import os
 import psycopg2
 import sensor
-import camera_manager
+import time
+import youtube_manager
+import yaml
 
 # Laps threshold
 threshold = 6
@@ -28,8 +32,55 @@ def is_active(last_sample, current_sample):
 def discard_video(filename):
     print "Discarding " + str(filename)
 
+def video_available(filename = 'video.yml'):
+    if !os.path.exists(filename): 
+        return False
+    video_date = None
+    video_url = None
+    video_is_uploaded = False
+    with open(filename, 'r') as f:
+        doc = yaml.load(f)
+        video_date = parser.parse(doc['date'])
+        video_url = doc['url']
+        video_is_uploaded = doc['uploaded'] != "False"
+
+    # We can't use an already uploaded video
+    if video_is_uploaded:
+        return True
+
+    # Check for a valid URL
+    if video_url is None or len(video_url) == 0:
+        return False
+
+    # Videos newer than 12 hours ago are valid
+    twelve_hours_ago = datetime.datetime.now() - datetime.datetime.timedelta(hours = 12)
+    if video_date is not None and (video_date > twelve_hours_ago):
+        return True
+
+def write_video_state(data, filename = 'video.yml'):
+    with open(filename, 'w') as outfile:
+        outfile.write(yaml.dump(data, default_flow_style=True))
+
+def attempt_upload(filename):
+    data = dict(
+        date = str(datetime.datetime.now()),
+        uploaded = "Uploading")
+    write_video_state(data)
+    result = youtube_manager.upload_video(filename)
+    if result is None or len(result) == 0:
+        data['uploaded'] = str(False)
+        write_video_state(data)
+        return False
+    data['uploaded'] = str(True)
+    data['url'] = result
+    write_video_state(data)
+
 def save_video(filename):
+    if video_available():
+        discard_video(filename)
+        return
     print "Saving " + str(filename)
+    attempt_upload(filename)
 
 if __name__ == '__main__':
     camera = camera_manager.CameraManager()
