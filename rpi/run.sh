22e8f432ab28797f1179f326c69fbb7e1a69c0ef
jlhonora <jlhonora@ing.puc.cl>
2015-03-28 12:00:30 +0000

    Modifications to run script

--- rpi/run.sh
+++ rpi/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start the redis server to manage the queue
-redis-server          &
+redis-server redis.conf &
 
 # Receives HTTP requests for data and fills the queue
 python -u api_manager.py &
@@ -10,8 +10,11 @@ python -u api_manager.py &
 python -u reader.py	&
 
 # Sends a tweet with the ran distance 
-python -u twitter_manager.py      &
+python -u twitter_manager.py &
 
 # The worker that reads from the redis queue and processes
 # the enqueued objects
-python -u worker.py              &
+python -u worker.py &
+
+# Creates videos continuously
+# sudo python -u camera_director.py &
